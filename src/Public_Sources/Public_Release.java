59fd6349c84ab8a482710f00e677937ce16cb0dc
saanc <sanukumargupta@gmail.com>
2015-04-08 17:17:21 +0530

    all the functions are updated and main function is more clear

--- src/Public_Sources/Public_Release.java
+++ src/Public_Sources/Public_Release.java
@@ -15,180 +15,202 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class Public_Release {
-static String song_name,movie_name,location;
-static int ind,flag,ind1;
-static String[] SongList,MovieList,SongLinkList;
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		String aug;
-		System.out.println("Enter the location where you want to save all the files:");
-		location=in.nextLine();
-		System.out.println("");
-		aug="Thank You!...Keep Your terminal in Full Screen Mode for better view";
-		for(int i=0;i<aug.length();i+=1){
-			System.out.print(aug.charAt(i));
-			delay(50);
-		}
-		System.out.println("");
-		aug="To Proceed...Press Enter";
-		for(int i=0;i<aug.length();i+=1){
-			System.out.print(aug.charAt(i));
-			delay(50);
-		}
-		 aug=in.nextLine();
+        static String songName,movieName,location;
+        static int ind,flag,ind1;
+        static String[] songList,movieList,songURLList;
+        static Scanner in=new Scanner(System.in);
+	
+        public static void main(String[] args) {
+		initialize();
 		while(true){
 			flag=0;
-			System.out.println("\n\t__________________________Welcome to InMD__________________________\n\t-------------------------------------------------------------------\n\t  more efficiency  more happiness  ^_^  ......Developed by saanc.\n\t-------------------------------------------------------------------\n\n");
-			System.out.println("\n\t===================================================================");
-			System.out.println("\n\tMenu:");
-			System.out.println("\n\t1) Download the song by song name and movie/album/artist name.\n\t2) Download the song by song name.\n\t3) Get list of songs of the movie.\n\t4) Top 10 Bollywood Releases.\n\t5) Make TODO list to download songs.");
-			System.out.println("\n\t===================================================================");
-			System.out.print("\n\nEnter your choice number from the Menu: ");
+			menuDisplay();
 			int choice=in.nextInt();
-			if(choice>5||choice<1){
-				System.out.println("\n\tWrong choice number!");
-				continue;
+			if(choice==1){
+                searchBySongAndMovieName();
 			}
-			if(choice==2){
-				String ToAvoidEnter=in.nextLine();
-				System.out.print("\nEnter the song name: ");
-				song_name=in.nextLine();
-				movie_name=null;
-				System.out.print("\n\n\tProcessing...");
-				get_song();
-			}
-			else if(choice==1){
-				String ToAvoidEnter=in.nextLine();
-				System.out.print("\nEnter the song name: ");
-				song_name=in.nextLine();
-				System.out.print("\nEnter the movie/Album/Artist name: ");
-				movie_name=in.nextLine();
-				System.out.print("\n\n\tProcessing...");
-				get_song();
+			else if(choice==2){
+                searchBySongName();
 			}
 			else if(choice==3){
-				String ToAvoidEnter=in.nextLine();
-				System.out.print("\nEnter the movie name: ");
-				movie_name=in.nextLine();
-				System.out.print("\n\n\tProcessing...");
-				GetListFromWiki();
-				if(ind==0){
-					AugmentedGetListFromWiki();
-				}
-				if(ind==0){
-					System.out.println("\n\tSorry! didn't find any result :(");
-					continue;
-				}
-				System.out.println("\n\tHere are the songs of movie: "+movie_name);
-				System.out.println("\n\t------------------------------------------------------");
-				int i;
-				for(i=1;i<=ind;i+=1){
-					System.out.println("\t"+i+") "+SongList[i-1]);
-				}
-				System.out.println("\n\t------------------------------------------------------");
-				System.out.print("\n\nHow many songs you wanna download from this list?: ");
-				int NoOfChoices=in.nextInt();
-				if(NoOfChoices>ind){
-					System.out.println("\n\tNumber of choices exceeded!");
-					continue;
-				}
-				System.out.println("\n\nEnter the song numbers that you wanna download separated by space\n");
-				int[] choices=new int[30];
-				for(i=0;i<NoOfChoices;i+=1){
-					choices[i]=in.nextInt();
-				}
-				
-				for(i=0;i<NoOfChoices;i+=1){
-					if(choices[i]>0&&choices[i]<=ind){
-						song_name=SongList[choices[i]-1];
-						System.out.print("\n\n\tProcessing...");
-					System.out.println("\n==================================================================");
-						System.out.println("\n\nDownloading the song \""+song_name+"\"...\n(Downloading might be slow due to your internet connection)");
-						System.out.print("\n\n\tPlease Wait....");
-						SaveUrlToFile(SongLinkList[choices[i]-1]);
-						if(flag==0)
-						System.out.println("\n\n\tDownloading Completed!\n\n");
-					System.out.println("\n==================================================================\n");
-					}
-				}
-				
+				songsFromMovie();		
 			}
-			
 			else if(choice==4){
-				System.out.print("\n\n\tProcessing...");
-				GetTop20();
-				if(ind==0){
-					System.out.println("Not Available");
-					continue;
-					}
-				System.out.println("\n\tHere are the top 10 bollywood songs: ");
-				System.out.println("\n\t------------------------------------------------------");
-				int i;
-				for(i=1;i<=ind;i+=1){
-					System.out.println("\t"+i+") "+SongList[i-1]+" - "+MovieList[i-1]);
-				}
-				System.out.println("\n\t------------------------------------------------------");
-				System.out.print("\n\nHow many songs you wanna download from this list?: ");
-				int NoOfChoices=in.nextInt();
-				if(NoOfChoices>ind){
-					System.out.println("\n\tNumber of choices exceeded!");
-					continue;
-				}
-				System.out.println("\n\nEnter the song numbers that you wanna download separated by space\n");
-				int[] choices=new int[30];
-				for(i=0;i<NoOfChoices;i+=1){
-					choices[i]=in.nextInt();
-				}
-				
-				for(i=0;i<NoOfChoices;i+=1){
-					if(choices[i]>0&&choices[i]<=ind){
-					song_name=SongList[choices[i]-1];
-					movie_name=MovieList[choices[i]-1];
-					System.out.print("\n\n\tProcessing...");
-					get_song();
-					}
-			}
+				top10();
 			}
 			else if(choice==5){
-				System.out.print("\nEnter TODO list size: ");
-				int TODOsize=in.nextInt();
-				String toAvoidEnter=in.nextLine();
-				SongList=new String[TODOsize];
-				MovieList=new String[TODOsize];
-				for(int i=0;i<TODOsize;i+=1){
-					System.out.print("\nEnter the name of song "+(i+1)+": ");
-					SongList[i]=in.nextLine();
-					System.out.print("\nEnter the name of movie "+(i+1)+": ");
-					MovieList[i]=in.nextLine();
-				}
-				System.out.println("\n\tHere is your TODO list: ");
-				System.out.println("\n\t------------------------------------------------------");
-				int i;
-				for(i=1;i<=TODOsize;i+=1){
-					System.out.println("\t"+i+") "+SongList[i-1]+" - "+MovieList[i-1]);
-				}
-				System.out.println("\n\t------------------------------------------------------");
-				
-				System.out.println("\n\tAll songs will automatically be downloaded soon :)\n");
-				for(i=0;i<TODOsize;i+=1){
-					movie_name=MovieList[i];
-					song_name=SongList[i];
-					System.out.print("\n\n\tProcessing...");
-					get_song();
-				}
+				todo();
 			}
 			else{
 				System.out.println("\n\tWrong input!");
 			}
 		}
 	}
+		
+	public static void initialize(){
+		System.out.println("Enter the location where you want to save all the files:");
+		location=in.nextLine();
+		System.out.println("");
+		String aug="Thank You!...Keep Your terminal in Full Screen Mode for better view\nTo Proceed...Press Enter";
+		for(int i=0;i<aug.length();i+=1){
+			System.out.print(aug.charAt(i));
+			delay(50);
+		}
+		 aug=in.nextLine();
+	}
+        
 	
-	public static void GetTop20(){
+	public static void menuDisplay(){
+		System.out.println("\n\t__________________________Welcome to InMD__________________________\n\t-------------------------------------------------------------------\n\t  more efficiency  more happiness  ^_^  ......Developed by saanc.\n\t-------------------------------------------------------------------\n\n");
+		System.out.println("\n\t===================================================================");
+		System.out.println("\n\tMenu:");
+		System.out.println("\n\t1) Download the song by song name and movie/album/artist name.\n\t2) Download the song by song name.\n\t3) Get list of songs of the movie.\n\t4) Top 10 Bollywood Releases.\n\t5) Make TODO list to download songs.");
+		System.out.println("\n\t===================================================================");
+		System.out.print("\n\nEnter your choice number from the Menu: ");
+	}
+        
+	public static void searchBySongAndMovieName(){
+		in.nextLine();
+		System.out.print("\nEnter the song name: ");
+		songName=in.nextLine();
+		System.out.print("\nEnter the movie/Album/Artist name: ");
+		movieName=in.nextLine();
+		System.out.print("\n\n\tProcessing...");
+		getSong();
+	}
+	
+	public static void searchBySongName(){
+		in.nextLine();
+		System.out.print("\nEnter the song name: ");
+		songName=in.nextLine();
+		movieName=null;
+		System.out.print("\n\n\tProcessing...");
+		getSong();
+	}
+	
+	public static void songsFromMovie(){
+		in.nextLine();
+		System.out.print("\nEnter the movie name: ");
+		movieName=in.nextLine();
+		System.out.print("\n\n\tProcessing...");
+		GetListFromWiki();
+		if(ind==0){
+			AugmentedGetListFromWiki();
+		}
+		if(ind==0){
+			System.out.println("\n\tSorry! didn't find any result :(");
+			return;
+		}
+		System.out.println("\n\tHere are the songs of movie: "+movieName);
+		System.out.println("\n\t------------------------------------------------------");
+		int i;
+		for(i=1;i<=ind;i+=1){
+			System.out.println("\t"+i+") "+songList[i-1]);
+		}
+		System.out.println("\n\t------------------------------------------------------");
+		System.out.print("\n\nHow many songs you wanna download from this list?: ");
+		int NoOfChoices=in.nextInt();
+		if(NoOfChoices>ind){
+			System.out.println("\n\tNumber of choices exceeded!");
+			return;
+		}
+		System.out.println("\n\nEnter the song numbers that you wanna download separated by space\n");
+		int[] choices=new int[30];
+		for(i=0;i<NoOfChoices;i+=1){
+			choices[i]=in.nextInt();
+		}
+		
+		for(i=0;i<NoOfChoices;i+=1){
+			if(choices[i]>0&&choices[i]<=ind){
+				songName=songList[choices[i]-1];
+				System.out.print("\n\n\tProcessing...");
+			System.out.println("\n==================================================================");
+				System.out.println("\n\nDownloading the song \""+songName+"\"...\n(Downloading might be slow due to your internet connection)");
+				System.out.print("\n\n\tPlease Wait....");
+				SaveUrlToFile(songURLList[choices[i]-1]);
+				if(flag==0)
+				System.out.println("\n\n\tDownloading Completed!\n\n");
+			System.out.println("\n==================================================================\n");
+			}
+		}
+	
+	}
+	
+	public static void top10(){
+		System.out.print("\n\n\tProcessing...");
+		getTop10();
+		if(ind==0){
+			System.out.println("Not Available");
+			return;
+			}
+		System.out.println("\n\tHere are the top 10 bollywood songs: ");
+		System.out.println("\n\t------------------------------------------------------");
+		int i;
+		for(i=1;i<=ind;i+=1){
+			System.out.println("\t"+i+") "+songList[i-1]+" - "+movieList[i-1]);
+		}
+		System.out.println("\n\t------------------------------------------------------");
+		System.out.print("\n\nHow many songs you wanna download from this list?: ");
+		int NoOfChoices=in.nextInt();
+		if(NoOfChoices>ind){
+			System.out.println("\n\tNumber of choices exceeded!");
+			return;
+		}
+		System.out.println("\n\nEnter the song numbers that you wanna download separated by space\n");
+		int[] choices=new int[30];
+		for(i=0;i<NoOfChoices;i+=1){
+			choices[i]=in.nextInt();
+		}
+		
+		for(i=0;i<NoOfChoices;i+=1){
+			if(choices[i]>0&&choices[i]<=ind){
+			songName=songList[choices[i]-1];
+			movieName=movieList[choices[i]-1];
+			System.out.print("\n\n\tProcessing...");
+			getSong();
+			}
+		}
+	}
+ 
+	
+	public static void todo(){
+		System.out.print("\nEnter TODO list size: ");
+		int TODOsize=in.nextInt();
+		in.nextLine();
+		songList=new String[TODOsize];
+		movieList=new String[TODOsize];
+		for(int i=0;i<TODOsize;i+=1){
+			System.out.print("\nEnter the name of song "+(i+1)+": ");
+			songList[i]=in.nextLine();
+			System.out.print("\nEnter the name of movie "+(i+1)+": ");
+			movieList[i]=in.nextLine();
+		}
+		System.out.println("\n\tHere is your TODO list: ");
+		System.out.println("\n\t------------------------------------------------------");
+		int i;
+		for(i=1;i<=TODOsize;i+=1){
+			System.out.println("\t"+i+") "+songList[i-1]+" - "+movieList[i-1]);
+		}
+		System.out.println("\n\t------------------------------------------------------");
+		
+		System.out.println("\n\tAll songs will automatically be downloaded soon :)\n");
+		for(i=0;i<TODOsize;i+=1){
+			movieName=movieList[i];
+			songName=songList[i];
+			System.out.print("\n\n\tProcessing...");
+			getSong();
+		}
+	}
+	
+	
+
+	
+	public static void getTop10(){
 		ind=0;
 		ind1=0;
 		Document doc;
-		SongList=new String[30];
-		MovieList=new String[30];
+		songList=new String[30];
+		movieList=new String[30];
 		try {
 			doc = Jsoup.connect("http://hindigeetmala.net/latest_top_10.htm").userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
 			//System.out.println(doc);
@@ -198,14 +220,14 @@ static String[] SongList,MovieList,SongLinkList;
 				if(song.contains("<a href=\"/song/")){
 					name=song.substring(song.indexOf(">")+2);
 					name=name.substring(0,name.indexOf("<")-1);
-					SongList[ind]=name;
+					songList[ind]=name;
 					ind+=1;
 				}
 				else if(song.contains("<a href=\"/movie/")){
 					//System.out.println(song);
 					name=song.substring(song.indexOf(">")+1);
 					name=name.substring(0,name.indexOf("<"));
-					MovieList[ind1]=name;
+					movieList[ind1]=name;
 					ind1+=1;
 				}
 			}
@@ -216,12 +238,11 @@ static String[] SongList,MovieList,SongLinkList;
 	}
 	
 	
-	
 	public static void AugmentedGetListFromWiki(){
 		String WikiLink=AugmentedGetWikiLinkFromGoogle();
 		//System.out.println(WikiLink);
-		SongList=new String[30];
-		SongLinkList=new String[30];
+		songList=new String[30];
+		songURLList=new String[30];
 		ind=0;
 		ind1=0;
 		Document doc;
@@ -236,9 +257,9 @@ static String[] SongList,MovieList,SongLinkList;
 ////				System.out.println(name);
 					name=name.substring(0,name.indexOf("-"));
 					name=name.replace("%20", " ");
-					SongList[ind]=name;
+					songList[ind]=name;
 					ind+=1;
-					SongLinkList[ind1]=song.substring(song.indexOf("\"")+1,song.indexOf(".mp3")+4);
+					songURLList[ind1]=song.substring(song.indexOf("\"")+1,song.indexOf(".mp3")+4);
 //					//System.out.println(SongLinkList[ind1]);
 					ind1+=1;
 				}
@@ -255,7 +276,7 @@ static String[] SongList,MovieList,SongLinkList;
 		Document doc;
 		String mod_link = null;
 		try {
-			doc = Jsoup.connect("http://www.google.co.in/search?q=site:downloadming.nu%20"+movie_name+"%20mp3%20"+"songs").userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
+			doc = Jsoup.connect("http://www.google.co.in/search?q=site:downloadming.nu%20"+movieName+"%20mp3%20"+"songs").userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
 			Elements links = doc.select("a[href]");
 			for (Element link : links)
 			{
@@ -277,8 +298,8 @@ static String[] SongList,MovieList,SongLinkList;
 	
 	public static void GetListFromWiki(){
 		String WikiLink=GetWikiLinkFromGoogle();
-		SongList=new String[30];
-		SongLinkList=new String[30];
+		songList=new String[30];
+		songURLList=new String[30];
 		ind=0;
 		ind1=0;
 		Document doc;
@@ -294,9 +315,9 @@ static String[] SongList,MovieList,SongLinkList;
 //				System.out.println(name);
 					name=name.substring(0,name.indexOf("<"));
 					//name=name.replace("%20", " ");
-					SongList[ind]=name;
+					songList[ind]=name;
 					ind+=1;
-					SongLinkList[ind1]=song.substring(song.indexOf("\"")+1,song.indexOf(".mp3")+4);
+					songURLList[ind1]=song.substring(song.indexOf("\"")+1,song.indexOf(".mp3")+4);
 //					//System.out.println(SongLinkList[ind1]);
 					ind1+=1;
 				}
@@ -312,7 +333,7 @@ static String[] SongList,MovieList,SongLinkList;
 		Document doc;
 		String mod_link = null;
 		try {
-			doc = Jsoup.connect("http://www.google.co.in/search?q=site:webmusic.in%20"+movie_name).userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
+			doc = Jsoup.connect("http://www.google.co.in/search?q=site:webmusic.in%20"+movieName).userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
 			Elements links = doc.select("a[href]");
 			for (Element link : links)
 			{
@@ -330,7 +351,7 @@ static String[] SongList,MovieList,SongLinkList;
 	}
 	
 	
-	public static void get_song(){
+	public static void getSong(){
 		System.out.println("\n==================================================================");
 		String download_link=crawl();
 		if(download_link==null||(!download_link.contains(".mp3"))){
@@ -338,10 +359,10 @@ static String[] SongList,MovieList,SongLinkList;
 			System.out.println("\n\tSorry! didn't find any result :(");
 		}
 		else{
-			if(movie_name!=null)
-			System.out.println("\n\nDownloading the song \""+song_name+"\"...\n(Downloading might be slow due to your internet connection)");
+			if(movieName!=null)
+			System.out.println("\n\nDownloading the song \""+songName+"\"...\n(Downloading might be slow due to your internet connection)");
 			else
-				System.out.println("\n\nDownloading the song \""+song_name+"\"...\n(Downloading might be slow due to your internet connection)");
+				System.out.println("\n\nDownloading the song \""+songName+"\"...\n(Downloading might be slow due to your internet connection)");
 			System.out.print("\n\n\tPlease Wait....");
 			SaveUrlToFile(download_link);
 			if(flag==0)
@@ -408,7 +429,7 @@ static String[] SongList,MovieList,SongLinkList;
 		Document doc;
 		String mod_link = null;
 		try {
-			doc = Jsoup.connect("http://www.google.co.in/search?q="+song_name+"+"+movie_name+"+"+"mp3skull").userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
+			doc = Jsoup.connect("http://www.google.co.in/search?q="+songName+"+"+movieName+"+"+"mp3skull").userAgent("Chrome").ignoreHttpErrors(true).timeout(0).get();
 			Elements links = doc.select("a[href]");
 			for (Element link : links)
 			{
@@ -432,7 +453,7 @@ static String[] SongList,MovieList,SongLinkList;
 		try {
 			URLConnection conn = new URL(download_link).openConnection();
 		    InputStream is = conn.getInputStream();
-		    OutputStream outstream = new FileOutputStream(new File(location+"/"+song_name+".mp3"));
+		    OutputStream outstream = new FileOutputStream(new File(location+"/"+songName+".mp3"));
 		    byte[] buffer = new byte[4092];
 		    int len;
 		    while ((len = is.read(buffer)) > 0) {
